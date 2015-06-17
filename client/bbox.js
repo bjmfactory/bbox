a7875f906c5b37cb2d7030731d2e919e0fb814dd
Ben Johnson <ben@bjmfactory.com>
2015-06-17 12:56:29 -0500

    saves links in db instead of array

--- client/bbox.js
+++ client/bbox.js
@@ -1,11 +1,10 @@
-if (Meteor.isClient) {
-  Template.body.helpers({
-    links: [
-      { title: "hello", url: "http://google.com"},
-      { title: "hello w", url: "http://google.com"},
-      { title: "hello world", url: "http://google.com"}
-    ]
-  });
-}
+Links = new Mongo.Collection("links");
+
+Template.body.helpers({
+  links: function() {
+    return Links.find({});
+  }
+});
+
 
 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+Links = new Mongo.Collection("links");
+
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
