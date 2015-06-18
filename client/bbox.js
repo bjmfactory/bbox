dbb4f14f464f9fd6d92b33bd9fdd022b67eb23b5
Ben Johnson <ben@bjmfactory.com>
2015-06-18 12:46:48 -0500

    adds new link form (which will be changed to edit)

--- client/bbox.js
+++ client/bbox.js
@@ -6,5 +6,22 @@ Template.body.helpers({
   }
 });
 
+Template.body.events({
+  "submit .new-link": function(event) {
+    var title = event.target.title.value;
+    var url   = event.target.url.value;
+    console.log("title: ", title)
+    console.log("url: ", url)
+
+    Links.insert({
+      title: title,
+      url: url,
+      createdAt: new Date()
+    });
+
+    return false;
+  }
+})
+
 
 
