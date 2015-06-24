315e268ee2cea5a8a66347db02acd5cd56c51729
Ben Johnson <ben@bjmfactory.com>
2015-06-23 21:05:04 -0500

    move link code to its own folder

--- client/links/links.js
+++ client/links/links.js
@@ -1,1 +1,10 @@
-// links.js
+Template.createLink.events({
+  "submit .createLink": function(event){
+    console.log("createLink called")
+    event.preventDefault();
+    var title = event.target.title.value;
+    var url   = event.target.url.value;
+    Meteor.call("createLink", title, url);
+    return false;
+  }
+})
