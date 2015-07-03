2426dd35c4dd7b560d7006a70d2171eb3681166c
Ben Johnson <ben@bjmfactory.com>
2015-07-03 10:17:57 -0500

    cleans creatLink(). removes log and resets form

--- client/links/links.js
+++ client/links/links.js
@@ -1,10 +1,14 @@
 Template.createLink.events({
   "submit .createLink": function(event){
-    console.log("createLink called")
     event.preventDefault();
+
     var title = event.target.title.value;
     var url   = event.target.url.value;
     Meteor.call("createLink", title, url);
+
+    event.target.title.value = '';
+    event.target.url.value   = '';
+
     return false;
   }
 })
