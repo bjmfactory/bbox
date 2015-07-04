27999cc49a157b9538bc03386a7aa8b0c60ada3c
Ben Johnson <ben@bjmfactory.com>
2015-07-03 19:16:31 -0500

    adds nested box/link templates with mock data and displays only 3 most recent

--- client/boxes/box.js
+++ client/boxes/box.js
@@ -1,7 +1,14 @@
 Meteor.subscribe("links");
 
 Template.boxes.helpers({
-  links: function() {
-    return Links.find({}, {sort: {createdAt: -1}, limit: 3}).fetch();
+  users: function() {
+    // mock data. this will be replaced with an array of user IDs
+    // when someone clicks "follow" on another persons profile page,
+    // it will unshift that ID to the front and cut one off the end
+    // if the array is longer than 9 (or should keep it and only display the first 9)?
+    return [{ username: "bjmfactory"}, { username: "benjamin" }, { username: "robert"}];
+  },
+  links: function(username) {
+    return Links.find({username: username}, {sort: {createdAt: -1}, limit: 3}).fetch();
   }
 });
